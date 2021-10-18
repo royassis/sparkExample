90c6486c4eeae5ede23ef22c14fdadd829ac866a
Roy Assis <royassisg@gmail.com>
2021-10-18 17:04:27 +0300

    new cmt

--- test_spark.py
+++ test_spark.py
@@ -9,7 +9,7 @@ from datetime import datetime
 # conf = pyspark.SparkConf()
 # conf.setMaster('spark://localhost:7077')
 # conf.set('spark.authenticate', False)
-# sc = pyspark.SparkContext(conf=conf)
+# sc = pyspark.SparkContext(conf=conf)P
 # .config("hive.metastore.uris", "localhost:9083")\
 
 
@@ -20,6 +20,7 @@ spark = SparkSession \
     .config("spark.sql.warehouse.dir", "/user/hive/warehouse") \
     .enableHiveSupport() \
     .getOrCreate()
+# spark = SparkSession.builder.master('local[*]').getOrCreate()
 
 # Use this to overwrite specific partition 
 spark.conf.set("spark.sql.sources.partitionOverwriteMode","dynamic")
@@ -34,14 +35,14 @@ spark.sql("use testdb")
 schema = StructType() \
       .add("id",IntegerType(),True) \
       .add("key",IntegerType(),True) \
-      .add("val",IntegerType(),True) \
+      .add("val",IntegerType(),True)
 
 
 # load csv
 df = spark.read.format("csv") \
       .option("header", True) \
       .schema(schema) \
-      .load("data.csv")
+      .load("file:////home/roy/scripts/python-projects/sparkTest/data.csv")
       # .load("hdfs://localhost:9000/tmp/data.csv")
 
 # show data
@@ -53,9 +54,9 @@ df.repartition(2,"key").write.option("header",True).partitionBy("key").mode('ove
 
 ## read parquet
 # fs
-parquetFile = spark.read.parquet("file:///home/roy/pycharmProject/sparkTest/data")
+parquetFile = spark.read.parquet("file:////home/roy/pycharmProject/sparkTest/data")
 # hdfs
-parquetFile = spark.read.parquet("/tmp/data")
+parquetFile = spark.read.parquet("file:////tmp/data")
 
 # show data
 parquetFile.show()
